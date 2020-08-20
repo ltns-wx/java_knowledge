a2f0f445297bc13cc3db6bcc5a985a2b824d24f4
wx <285476359@qq.com>
2020-08-20 21:42:14 +0800

    数据结构

--- JavaDataStructure/src/cm/ltns/structure/linear_线性结构/linearlist_线性表/ArrayLinearList_链式实现方式/LinkedSinglyCircularList.java
+++ JavaDataStructure/src/cm/ltns/structure/linear_线性结构/linearlist_线性表/ArrayLinearList_链式实现方式/LinkedSinglyCircularList.java
@@ -24,65 +24,90 @@ public class LinkedSinglyCircularList<E> {
 
     // 构造函数
     public LinkedSinglyCircularList() {
-        head = new Node();
-        head.next = head;
+       head = null;
         size = 0;
     }
 
     // 尾部插入
-    public void add(E e) {
-        Node cur = this.head;
-        // 找出最后一个节点
-        while (cur.next != this.head) {
-            cur = cur.next;
-        }
-        Node node = new Node(e, null);
-        node.next = cur.next;
-        cur.next = node;
-        size ++ ;
-    }
-
-
+//    public void addEnd(E e) {
+//        Node cur = this.head;
+//
+//        // 找出最后一个节点
+//        while (cur.next != this.head) {
+//            cur = cur.next;
+//        }
+//        Node node = new Node(e, null);
+//        if (size == 0) {
+//            head = node;
+//            node.next = head;
+//        } else {
+//            // 先把当前节点 赋给 新增加节点的下一个节点（这样就不会出现问题）
+//            node.next = cur.next;
+//            // 然后再把新节点赋给当前节点
+//            cur.next = node;
+//        }
+//        size++;
+//    }
 
     // 头插法
-    public void add(int index, E e) {
-        if (index < 0 || index > size + 1) {
-            System.out.println("下标索引越界");
-        }
-        int cur = 0;
-        Node p = head;
-        Node q = head.next;
-        while (cur < index - 1) { //获取index前一个节点
-            p = q;
-            q = p.next;
-            cur++;
-        }
-        // p 为指定位置上的元素
-        Node node = new Node(e, null);
-        node.setNext(q);
-        p.next = node;
-        size ++;
-    }
+//    public void addStart(E e) {
+//        Node node = new Node(e, null);
+//        // 尾部节点
+//        Node cur = this.head;
+//        // 找出最后一个节点
+//        while (cur.next != this.head) {
+//            cur = cur.next;
+//        }
+//        if (size == 0) {
+//            head = node;
+//            node.next = head;
+//        } else {
+//            node.next = head;
+//            head = node;
+//            cur.next = head;
+//        }
+//        size++;
+//    }
 
-    //任意位置插入，第一个节点为0
+
+    //插入指定索引位置（相当于移动指针）
     public boolean addIndex(int index, E data) {
         if (index < 0 || index > size + 1) {
             System.out.println("下标索引越界");
         }
-        Node curNode = head;    // 当前元素
-        for (int i = 0; i < index; i++) {
-            curNode = curNode.next;
+        Node lastNode = head;    // 尾部节点
+        for (int i = 0; i < size; i++) {
+            lastNode = lastNode.next;
         }
-        //cur就是index位置的前驱
-        Node node = new Node(data,null);
-        node.next = curNode.next;
-        curNode.next = node;
-        size ++;
+        Node node = new Node(data, null);
+        boolean isEmpty = size == 0 && head == null && lastNode == null;
+        if (isEmpty) {
+            head = node;
+            lastNode = node;
+            lastNode.next = head;
+        } else if (index == 0) {
+            node.next = head;
+            head = node;
+            lastNode.next = head;
+        } else if (index == size) { // 表尾添加元素
+            node.next = lastNode.next;
+            lastNode.next = node;
+            lastNode = node;   // 类似于指针移动
+        }else{
+            Node p = head ;
+            for (int i = 0; i < index-1; i++) {
+                p = p.next;
+            }
+            node.next =p.next;
+            p.next =node ;
+        }
+        size++;
         return true;
     }
-        // 指定索引
+    // 指定索引
 
     // 删除
+
 
     //插叙
 
@@ -93,7 +118,7 @@ public class LinkedSinglyCircularList<E> {
         Node p = head;
         Node q = head.next;
         sb.append("head=").append(head.getData());
-        for (int i = 1; i <= size; i++) {
+        for (int i = 1; i < size; i++) {
             sb.append("-->" + q.getData());
             p = q;
             q = p.getNext();
@@ -103,8 +128,8 @@ public class LinkedSinglyCircularList<E> {
 
     public static void main(String[] args) {
         LinkedSinglyCircularList<String> linkedSinglyCircularList = new LinkedSinglyCircularList();
-        linkedSinglyCircularList.addIndex(1,"a");
-        linkedSinglyCircularList.addIndex(1,"b");
+        linkedSinglyCircularList.addIndex(0,"a");
+        linkedSinglyCircularList.addIndex(0,"b");
         linkedSinglyCircularList.addIndex(2,"c");
         System.out.println(linkedSinglyCircularList);
 
---
+++ JavaDataStructure/src/cm/ltns/structure/linear_线性结构/linearlist_线性表/ArrayLinearList_链式实现方式/MyHeadSingleList.java
@@ -0,0 +1,163 @@
+package cm.ltns.structure.linear_线性结构.linearlist_线性表.ArrayLinearList_链式实现方式;
+
+/**
+ * 这种事有head头的
+ */
+public class MyHeadSingleList {
+    class Node {  //内部节点类
+        private int data;  //节点数据
+        private Node next;  //下一个节点引用
+
+        public Node(int data) {  //带参数的构造方法，用于实例化普通节点
+            this.data = data;
+            this.next = null;
+        }
+
+        //头结点
+        public Node() {  //不带参数的构造方法，用于实例化头节点
+            this.data = -1;
+            this.next = null;
+        }
+    }
+
+    private Node head;//声明头节点
+
+    public MyHeadSingleList() {  //外部类无参构造方法
+        this.head = new Node();   //实例化头节点
+        this.head.next = this.head;  //构成循环链表
+    }
+
+    //头插法
+    public void addFirst(int data) {
+        Node node = new Node(data);
+        node.next = this.head.next;
+        this.head.next = node;
+    }
+
+    //尾插法
+    public void addLast(int data) {
+        Node cur = this.head;
+        while (cur.next != this.head) {
+            cur = cur.next;
+        }
+        Node node = new Node(data);
+        node.next = cur.next;
+        cur.next = node;
+    }
+
+    //检验位置合法性
+    private void checkIndex(int index) {
+        if (index < 0 || index > getLength()) {
+            throw new UnsupportedOperationException("index位置不合法");
+        }
+    }
+
+    //任意位置插入，第一个节点为0
+    public boolean addIndex(int index, int data) {
+        checkIndex(index);
+        Node cur = this.head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        //cur就是index位置的前驱
+        Node node = new Node(data);
+        node.next = cur.next;
+        cur.next = node;
+        return true;
+    }
+
+    //是否包含关键字key
+    public boolean contains(int key) {
+        Node cur = this.head.next;//head没有数据，从第一个节点开始查找
+        while (cur != this.head) {
+            if (cur.data == key) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    //查找节点前驱
+    private Node searchPrev(int key) {
+        Node cur = this.head;
+        while (cur.next != this.head) {//从头节点开始遍历
+            if (cur.next.data == key) {
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    //删除第一次出现的关键字key
+    public int remove(int key) {
+        Node prev = searchPrev(key);
+        if (prev == null) {
+            throw new UnsupportedOperationException("没有key的前驱");
+        }
+        int oldData = 0;
+        Node del = prev.next;
+        oldData = del.data;
+        prev.next = del.next;
+        return oldData;
+    }
+
+
+    //删除所有值为key的关键字
+    public void removeAllKey(int key) {
+        if (this.head == null || this.head.next == this.head) {
+            return;
+        }
+        Node prev = this.head;
+        Node cur = this.head.next;
+        while (cur != this.head) {
+            if (cur.data == key) {
+                prev.next = cur.next;
+                cur = prev.next;
+            } else {
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+    }
+
+    //获取链表的长度
+    public int getLength() {
+        int count = 0;
+        Node cur = this.head.next;//链表的长度不包括头节点
+        while (cur != this.head) {
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
+
+    //打印链表元素
+    public void display() {
+        Node cur = this.head.next;//不需要打印头节点
+        while (cur != this.head) {
+            System.out.print(cur.data + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    //清空链表
+    public void clear() {
+        while (this.head.next != this.head) {
+            Node cur = this.head.next;
+            this.head.next = cur.next;
+        }
+        this.head = null;
+
+    }
+
+    public static void main(String[] args) {
+        MyHeadSingleList myHeadSingleList = new MyHeadSingleList();
+        myHeadSingleList.addLast(1);
+        myHeadSingleList.addLast(2);
+        myHeadSingleList.addLast(3);
+        myHeadSingleList.display();
+    }
+}
